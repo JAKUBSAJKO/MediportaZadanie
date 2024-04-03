ca2563713a4913ad795f53f22df4507859e09217
Jakub Sajko <jakub.s@futuresight.com>
2024-04-03 20:27:05 +0200

    Fetch data and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { Container } from "@mui/material";
+import useSWR from "swr";
+import { baseURL } from "./constants";
+import { Tag } from "./types";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function App() {
-  return <div>App</div>;
+  const { data, error, isLoading } = useSWR(
+    `${baseURL}/tags?page=1&pagesize=10&order=desc&sort=popular&site=stackoverflow`,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <Container maxWidth="xl" sx={{ padding: "64px 0" }}>
+      {data &&
+        data.items.map((item: Tag) => (
+          <div key={item.name}>
+            <h1>{item.name}</h1>
+            <p>{item.count}</p>
+          </div>
+        ))}
+    </Container>
+  );
 }
